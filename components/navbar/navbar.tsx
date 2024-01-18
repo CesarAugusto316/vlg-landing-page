3c415df8a6e35a4140a2f449c2cbb2c2c44e2088
cesaraugusto316 <cesaraugusto316@protonmail.com>
2024-01-17 23:05:07 -0500

    ✨ Update global and navbar styles

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import Link from 'next/link'
 import styles from './navbar.module.scss'
 
 
@@ -6,8 +7,18 @@ export const Navbar: FC = () => {
   return (
     <nav className={styles.container}>
       <ul>
-        <li>Mexico</li>
-        <li>Chile</li>
+        <li>
+          <Link href="/" className={styles.link}>Inicio</Link>
+        </li>
+        <li>
+          <Link href="/mexico" className={styles.link}>Mexico</Link>
+        </li>
+        <li>
+          <Link href="/chile" className={styles.link}>Chile</Link>
+        </li>
+        <li>
+          <Link href="/contacts" className={styles.link}>Contactos</Link>
+        </li>
       </ul>
     </nav>
   )
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  reactStrictMode: false,
+  // i18n: {
+  //   // localeDetection: true,
+  // }
 }
 
 module.exports = nextConfig
