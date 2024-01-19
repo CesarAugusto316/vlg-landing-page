068e83035e0315c02afc56c78a4392a358b73b05
cesaraugusto316 <cesaraugusto316@protonmail.com>
2024-01-18 23:03:01 -0500

    ✨ Add Card component and VlgChile page

--- app/services/chile/page.tsx
+++ app/services/chile/page.tsx
@@ -1,4 +1,6 @@
+import { Card } from '@/components/index'
 import styles from './page.module.scss'
+import img1 from '@/assets/image 21.png'
 
 
 export default function Chile(): JSX.Element {
@@ -9,6 +11,15 @@ export default function Chile(): JSX.Element {
         <h1>Victoria Line Group - Chile</h1>
 
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos; s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type</p>
+
+        <div>
+          <Card
+            title="VLG Chile"
+            img={img1}
+            description="Where can I get some? Lorem Ipsum is simply dummy"
+            link="/services/chile/vlg-chile"
+          />
+        </div>
       </section>
     </div>
   )
---
+++ components/card/card.tsx
@@ -0,0 +1,31 @@
+import { FC } from 'react';
+import styles from './card.module.scss'
+import Image, { StaticImageData } from 'next/image';
+import { Button } from '@/components/index';
+import Link from 'next/link';
+
+
+interface CardProps {
+  title: string,
+  img: StaticImageData,
+  description: string,
+  link: string
+}
+
+export const Card: FC<CardProps> = ({ description, img, link, title }) => {
+  return (
+    <div className={styles.card}>
+
+      <h4 className={styles.h4}>{title}</h4>
+
+      <Image src={img} alt={title} />
+
+      <p>{description}</p>
+
+      <Link href={link}>
+        <Button variant="light">Ver más...</Button>
+      </Link>
+
+    </div>
+  )
+}
