51d3d2dc070dcedc58e128f7b7dfcf514ac1fe50
cesaraugusto316 <cesaraugusto316@protonmail.com>
2024-01-18 23:30:33 -0500

    ✨ Refactor Chile page to use cardData array for rendering cards

--- app/services/chile/page.tsx
+++ app/services/chile/page.tsx
@@ -5,6 +5,43 @@ import img2 from '@/assets/image 22.png'
 import img3 from '@/assets/image 23.png'
 
 
+const cardData = [
+  {
+    title: 'VLG Chile',
+    img: img1,
+    description: (
+      <>
+        <b> Where can I get some?</b>
+        <br /> Lorem Ipsum is simply dummy
+      </>
+    ),
+    link: '/services/chile/vlg-chile',
+  },
+  {
+    title: 'VLG Storage & Logistic',
+    img: img2,
+    description: (
+      <>
+        <b>Why do we use it?</b>
+        <br /> Lorem Ipsum is simply dummy
+      </>
+    ),
+    link: '/services/chile/vlg-chile',
+  },
+  {
+    title: 'VLG Agrominerals',
+    img: img3,
+    description: (
+      <>
+        Line Group Lorem Ipsum <br />
+        <b>is simply dummy</b>
+      </>
+    ),
+    link: '/services/chile/vlg-chile',
+  },
+];
+
+
 export default function Chile(): JSX.Element {
   return (
     <div className={styles.container}>
@@ -15,24 +52,15 @@ export default function Chile(): JSX.Element {
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos; s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type</p>
 
         <div className={styles.cards}>
-          <Card
-            title="VLG Chile"
-            img={img1}
-            description={<><b> Where can I get some?</b><br />Lorem Ipsum is simply dummy</>}
-            link="/services/chile/vlg-chile"
-          />
-          <Card
-            title="VLG Storage & Logistic"
-            img={img2}
-            description={<><b>Why do we use it?</b><br /> Lorem Ipsum is simply dummy</>}
-            link="/services/chile/vlg-chile"
-          />
-          <Card
-            title="VLG Agrominerals"
-            img={img3}
-            description={<>Line Group Lorem Ipsum <br /> <b>is simply dummy</b></>}
-            link="/services/chile/vlg-chile"
-          />
+          {cardData.map((card, index) => (
+            <Card
+              key={index}
+              title={card.title}
+              img={card.img}
+              description={card.description}
+              link={card.link}
+            />
+          ))}
         </div>
       </section>
     </div>
