7214470f09ebb6391c904fbdee35471b2db17667
cesaraugusto316 <cesaraugusto316@protonmail.com>
2024-01-18 21:17:14 -0500

    ✨ Update styles in page, button, footer, and navbar modules

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,11 +1,72 @@
 import { FC } from 'react'
 import styles from './footer.module.scss'
+import logo from '@/assets/vlg_logo470x470.png'
+import chileFlag from '@/assets/chile_flag.png'
+import mexicoFlag from '@/assets/mexico_flag.png'
+import instagram from '@/assets/instagram.png'
+import facebook from '@/assets/facebook.png'
+import linkedin from '@/assets/linkedin.png'
+import twitter from '@/assets/twitter.png'
+import Image from 'next/image'
+import Link from 'next/link'
 
 
 export const Footer: FC = () => {
   return (
     <footer className={styles.container}>
-      <p>Footer</p>
+
+      <div className={styles.col1}>
+        <h3>Victoria Line Group</h3>
+        <p>2024 @ VictoriaLineGroup <br />
+          Todos los derechos reservados.
+        </p>
+
+        <ul>
+          <li>
+            <Image src={facebook} alt="insta-logo" />
+          </li>
+          <li>
+            <Image src={instagram} alt="insta-logo" />
+          </li>
+          <li>
+            <Image src={linkedin} alt="insta-logo" />
+          </li>
+          <li>
+            <Image src={twitter} alt="insta-logo" />
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles.col2}>
+        <ul>
+          <li>
+            <Link href="/" className={styles.link}>Inicio</Link>
+          </li>
+          <li>
+            <Link href="/contacts" className={styles.link}>Contactos</Link>
+          </li>
+          <li>
+            <Link href="/contacts" className={styles.link}>Seguimientos</Link>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <Link href="/chile" className={styles.link}>
+              <Image src={chileFlag} alt="chile-flag" />
+              <span>VGL Chile</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/mexico" className={styles.link}>
+              <Image src={mexicoFlag} alt="mexico-flag" />
+              <span>VGL Mexico</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <Image className={styles.logo} src={logo} alt="vlg-log" />
     </footer>
   )
 }
