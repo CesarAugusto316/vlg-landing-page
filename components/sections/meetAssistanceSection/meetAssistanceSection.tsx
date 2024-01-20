787353963e936230fae04ebcb15dc6ecaf7cdc07
cesaraugusto316 <cesaraugusto316@protonmail.com>
2024-01-19 21:23:07 -0500

    ✨ Update styles and content in globals.scss, footer.module.scss, meetAssistanceSection.module.scss, and meetAssistanceSection.tsx

--- components/sections/meetAssistanceSection/meetAssistanceSection.tsx
+++ components/sections/meetAssistanceSection/meetAssistanceSection.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import styles from './meetAssistanceSection.module.scss';
 import Image, { StaticImageData } from 'next/image';
+import Link from 'next/link';
 
 
 interface MeetAssistanceSectionProps {
@@ -18,18 +19,24 @@ export const MeetAssistanceSection: FC<MeetAssistanceSectionProps> = (props) => 
 
   return (
     <section className={styles.meetAssistants}>
-      <div>
-        <h4>{title}</h4>
+      <h4>{title}</h4>
+
+      <div className={styles.avatarContainer}>
+
+        <div>
+          <h5>{name}</h5>
+          <p>{job}</p>
+          <Link href={'#'}>{email}</Link>
+        </div>
 
         <Image src={avatar} alt={`avatar-${name}`} />
 
-        <p>{name}</p>
-        <p>{email}</p>
-        <p>{job}</p>
-        <p>{phone}</p>
-        <p>{meetLink}</p>
-
+        <div>
+          <p>{phone}</p>
+          <Link href={meetLink}>Agendar</Link>
+        </div>
       </div>
+
     </section>
   )
 }
