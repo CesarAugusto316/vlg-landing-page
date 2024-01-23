26ecf105fdda4a7fe1931f4c815000da9aef6a23
cesaraugusto316 <cesaraugusto316@protonmail.com>
2024-01-22 20:26:00 -0500

    ✨ Update loading skeleton dimensions and shapes

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,20 +4,15 @@ import ContentLoader from 'react-content-loader'
 
 const LoadingSkeleton = () => {
   return (
-    <ContentLoader speed={2}
-      width={300}
-      height={170}
-      viewBox="0 0 400 170"
+    <ContentLoader
+      viewBox="0 0 500 500"
+      height={340}
+      width={340}
       backgroundColor="#f3f3f3"
       foregroundColor="#ecebeb"
     >
-      <circle cx="248" cy="59" r="49" />
-      <circle cx="263" cy="66" r="8" />
-      <rect x="175" y="120" rx="0" ry="0" width="156" height="8" />
-      <rect x="204" y="137" rx="0" ry="0" width="100" height="8" />
-      <rect x="248" y="128" rx="0" ry="0" width="0" height="1" />
-      <rect x="247" y="126" rx="0" ry="0" width="1" height="8" />
-      <rect x="252" y="166" rx="0" ry="0" width="1" height="0" />
+      <rect x="10" y="10" rx="10" ry="10" width="480" height="480" />
+      <ellipse cx="250" cy="250" rx="100" ry="100" />
     </ContentLoader>
   )
 }
