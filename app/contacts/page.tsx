08c034078a8c228b6ceb107325b51c79fa37897b
cesaraugusto316 <cesaraugusto316@protonmail.com>
2024-01-22 22:46:43 -0500

    ✨ Add message sent feature to Contacts form

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -5,6 +5,7 @@ import { Formik, Form, Field, FormikHelpers } from 'formik';
 import Image from 'next/image';
 import img from '@/assets/contacts.png'
 import * as Yup from 'yup';
+import { useEffect, useState } from 'react';
 
 
 interface FormValues {
@@ -27,17 +28,26 @@ const validationSchema = Yup.object<FormValues>({
 
 
 export default function Contacts(): JSX.Element {
+  const [isSent, setIsSent] = useState(false);
 
   const handleFormSubmision =
     async (values: FormValues, actions: FormikHelpers<FormValues>) => {
       try {
         console.log(values)
         actions.setSubmitting(false)
+        setIsSent(true) // show the message sent
       }
       catch (error) {
         console.log(error)
       }
     }
+
+  useEffect(() => {
+    // cleanup when the form is unmounted, restore the initial value
+    return () => {
+      setIsSent(false)
+    }
+  }, [])
 
   return (
     <HeroSection>
@@ -45,38 +55,46 @@ export default function Contacts(): JSX.Element {
         <div>
           <Image src={img} alt="image-contacts" />
         </div>
+        {isSent ? (
+          <div className={styles.messageSent}>
+            <h1>Gracias por tu mensaje</h1>
+            <p>Pronto estaremos en contacto!</p>
 
-        <Formik
-          validationSchema={validationSchema}
-          initialValues={initialValues}
-          onSubmit={handleFormSubmision}
-        >
-          {() => (
-            <Form className={styles.form}>
-              <ul>
-                <li>
-                  <h1>Contáctanos</h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                </li>
-                <li>
-                  <label htmlFor="name">Nombre</label>
-                  <Field placeholder="Nombre completo" className={styles.input} type="text" name="name" id="name" />
-                </li>
-                <li>
-                  <label htmlFor="email">Email</label>
-                  <Field placeholder="Dirección Email" className={styles.input} type="email" name="email" id="email" />
-                </li>
-                <li>
-                  <label htmlFor="message">Mensage</label>
-                  <Field placeholder="¿En qué podemos ayudarte?" className={styles.input} rows={2} as="textarea" name="message" id="message" />
-                </li>
-                <li>
-                  <Button variant="primary">Enviar mensaje</Button>
-                </li>
-              </ul>
-            </Form>
-          )}
-        </Formik>
+            <Button variant="primary">Volver al inicio</Button>
+          </div>
+        ) : (
+          <Formik
+            validationSchema={validationSchema}
+            initialValues={initialValues}
+            onSubmit={handleFormSubmision}
+          >
+            {() => (
+              <Form className={styles.form}>
+                <ul>
+                  <li>
+                    <h1>Contáctanos</h1>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                  </li>
+                  <li>
+                    <label htmlFor="name">Nombre</label>
+                    <Field placeholder="Nombre completo" className={styles.input} type="text" name="name" id="name" />
+                  </li>
+                  <li>
+                    <label htmlFor="email">Email</label>
+                    <Field placeholder="Dirección Email" className={styles.input} type="email" name="email" id="email" />
+                  </li>
+                  <li>
+                    <label htmlFor="message">Mensage</label>
+                    <Field placeholder="¿En qué podemos ayudarte?" className={styles.input} rows={2} as="textarea" name="message" id="message" />
+                  </li>
+                  <li>
+                    <Button variant="primary">Enviar mensaje</Button>
+                  </li>
+                </ul>
+              </Form>
+            )}
+          </Formik>
+        )}
       </div>
     </HeroSection>
   )
