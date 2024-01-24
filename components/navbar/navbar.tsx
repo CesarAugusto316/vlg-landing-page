f00a94a14ba7341b7185f001eef86feecd58322b
cesaraugusto316 <cesaraugusto316@protonmail.com>
2024-01-24 17:39:34 -0500

    ✨ Refactor navbar styling and add mobile dropdown

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -16,47 +16,48 @@ export const Navbar: FC = () => {
   const [toggleButton, setToggleButton] = useState(false);
 
   return (
-    <nav className={styles.container}>
+    <div className={styles.wrapper}>
+      <nav className={styles.container}>
+        <section className={styles.navigation}>
+          <Image className={styles.logo} src={logo} alt="vlg-logo" />
 
-      <section className={styles.navigation}>
-        <Image className={styles.logo} src={logo} alt="vlg-logo" />
+          <ul>
+            <li>
+              <Link href="/" className={styles.link}>Inicio</Link>
+            </li>
+            <li>
+              <Link href="/services/chile" className={styles.link}>
+                <Image src={chileFlag} alt="chile-flag" />
+                <span>VGL Chile</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/services/mexico" className={styles.link}>
+                <Image src={mexicoFlag} alt="mexico-flag" />
+                <span>VGL Mexico</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/contacts" className={styles.link}>Contactos</Link>
+            </li>
+          </ul>
+        </section>
 
-        <ul>
-          <li>
-            <Link href="/" className={styles.link}>Inicio</Link>
-          </li>
-          <li>
-            <Link href="/services/chile" className={styles.link}>
-              <Image src={chileFlag} alt="chile-flag" />
-              <span>VGL Chile</span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/services/mexico" className={styles.link}>
-              <Image src={mexicoFlag} alt="mexico-flag" />
-              <span>VGL Mexico</span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/contacts" className={styles.link}>Contactos</Link>
-          </li>
-        </ul>
-      </section>
+        <Link href="#" className={styles.followUp}>
+          <Button variant="light">Seguimiento</Button>
+        </Link>
 
-      <Link href="#" className={styles.followUp}>
-        <Button variant="light">Seguimiento</Button>
-      </Link>
+        <button onClick={() => setToggleButton(prev => !prev)} className={styles.bars}>
+          {toggleButton ?
+            <Image src={xIcon} alt="x-icon" />
+            :
+            <Image src={bars} alt="bars" />
+          }
+        </button>
+      </nav>
 
       <MobileDropDown isOpen={toggleButton} />
-
-      <button onClick={() => setToggleButton(prev => !prev)} className={styles.bars}>
-        {toggleButton ?
-          <Image src={xIcon} alt="x-icon" />
-          :
-          <Image src={bars} alt="bars" />
-        }
-      </button>
-    </nav>
+    </div>
   )
 }
 
@@ -68,7 +69,7 @@ const MobileDropDown: FC<MobileDropDownProps> = ({ isOpen }) => {
   const transition = useTransition(isOpen, {
     from: {
       y: '-100%',
-      opacity: 0.4,
+      opacity: 0,
     },
     enter: {
       y: '0%',
@@ -79,37 +80,41 @@ const MobileDropDown: FC<MobileDropDownProps> = ({ isOpen }) => {
       opacity: 0,
     },
     config: {
-      tension: 180,
+      tension: 220,
       mass: 1.2
     }
   });
 
   return transition((style, isOpen) => (
     isOpen &&
-    <animated.ul style={style} className={styles.mobileDropdown}>
-      <li>
-        <Link href="/" className={styles.link}>Inicio</Link>
-      </li>
-      <li>
-        <Link href="/services/chile" className={styles.link}>
-          <Image src={chileFlag} alt="chile-flag" />
-          <span>VGL Chile</span>
-        </Link>
-      </li>
-      <li>
-        <Link href="/services/mexico" className={styles.link}>
-          <Image src={mexicoFlag} alt="mexico-flag" />
-          <span>VGL Mexico</span>
-        </Link>
-      </li>
-      <li>
-        <Link href="/contacts" className={styles.link}>Contactos</Link>
-      </li>
-      <li>
-        <Link href="#">
-          <Button variant="light">Seguimiento</Button>
-        </Link>
-      </li>
-    </animated.ul>
+    <>
+      <animated.ul style={{ transform: style.y.to(y => `translateY(${y})`) }} className={styles.mobileDropdown}>
+        <li>
+          <Link href="/" className={styles.link}>Inicio</Link>
+        </li>
+        <li>
+          <Link href="/services/chile" className={styles.link}>
+            <Image src={chileFlag} alt="chile-flag" />
+            <span>VGL Chile</span>
+          </Link>
+        </li>
+        <li>
+          <Link href="/services/mexico" className={styles.link}>
+            <Image src={mexicoFlag} alt="mexico-flag" />
+            <span>VGL Mexico</span>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contacts" className={styles.link}>Contactos</Link>
+        </li>
+        <li>
+          <Link href="#">
+            <Button variant="light">Seguimiento</Button>
+          </Link>
+        </li>
+      </animated.ul>
+      <animated.div style={{ opacity: style.opacity }} className={styles.backdrop}>
+      </animated.div>
+    </>
   ))
 }
